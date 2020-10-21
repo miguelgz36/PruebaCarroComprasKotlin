e6bce9dc46669f9adf2082fb2930a6929c43a542
Miguel Angel Beltrán Rodríguez <68168635+miguelgoz@users.noreply.github.com>
2020-10-21 14:50:19 -0500

    proyect created

---
+++ src/main/kotlin/com/carrito/carrito/CarritoComprasApplication.kt
@@ -0,0 +1,11 @@
+package com.carrito.carrito
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CarritoComprasApplication
+
+fun main(args: Array<String>) {
+	runApplication<CarritoComprasApplication>(*args)
+}
